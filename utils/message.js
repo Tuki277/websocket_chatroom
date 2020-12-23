a137ebb0d9f643f422b9fe6b6806aed6e57329b7
Tran Hoang Huy <huybeos2707@gmail.com>
2020-12-23 22:49:30 +0700

    config time

--- utils/message.js
+++ utils/message.js
@@ -1,10 +1,12 @@
-const moment = require('moment');
+// const moment = require('moment');
+var moment = require('moment-timezone');
 
 function formatMessage (username, text) {
     return {
         username,
         text,
-        time: moment().format(' h:mm a')
+        // time: moment().format(' h:mm a')
+        time : moment().tz("Asia/Ho_Chi_Minh").format(' h:mm a')
     };
 }
 
